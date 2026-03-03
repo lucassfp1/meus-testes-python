f522fa276a8715ff68c7f1b00c52d8a050b61148
Lucas Paim <lucasferpa1985@gmail.com>
2026-03-02 22:35:56 -0300

    adiçao do novo ex

---
+++ ex/ex005.py
@@ -0,0 +1,18 @@
+#desafio 14
+
+g = float(input("Quantos graus está? "))
+f = (g*9/5)+32
+print("-"*40)
+print(f"A temperatura de {g} graus Celsius \nserá convertida em {f:.2f} grau Fahrenheit!")
+print("-"*40)
+
+#desafio 15
+
+km = float(input("Qual a quantidade de Km percorrido? "))
+dias = int(input("Qual a quantidade de dias alugados? "))
+pkm = km*0.15
+pdias = dias*60
+print("-"*50)
+print(f"Pelo aluguel de {dias} dias e pela distancia de {km} KM")
+print(f"O preço total a pagar será R${pkm+pdias:.2f}")
+print("-"*50)
